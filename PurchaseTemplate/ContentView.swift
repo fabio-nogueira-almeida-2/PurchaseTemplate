8ae0e659f08d35f327614cabdb380fa5f502d2fe
Fábio Nogueira de Almeida <fabio.nogueira@sumup.com>
2025-11-07 20:00:00 -0300

    Initial Commit

---
+++ PurchaseTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PurchaseTemplate
+//
+//  Created by Fabio Nogueira de Almeida on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PurchaseTemplate/PurchaseTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurchaseTemplateApp.swift
+//  PurchaseTemplate
+//
+//  Created by Fabio Nogueira de Almeida on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PurchaseTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
